29d9a1cf6d570d7e826cdeb2f1bf37469ed7049d
arsproger <arsproger@gmail.com>
2022-12-08 20:15:06 +0600

    Add project

---
+++ src/AbstractNote.java
@@ -0,0 +1,6 @@
+import java.time.LocalDate;
+
+public abstract class Note {
+    private String text;
+    private LocalDate localDate;
+}
---
+++ src/TypeNote.java
@@ -0,0 +1,3 @@
+public enum TYPENOTE {
+    IDEAS, DUTY
+}
